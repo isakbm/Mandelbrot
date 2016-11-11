4af10587458cc4a00cf01f09c45200b7044987ef
Andrew Leitch <aleitch020@gmail.com>
2016-11-11 19:04:43 +0100

    Lower GLFW_CONTEXT_VERSION: 4.0 => 3.3

--- main.cpp
+++ main.cpp
@@ -241,8 +241,8 @@ void initGL() {
         exit(-1);
     }
     glfwWindowHint(GLFW_SAMPLES, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     window = glfwCreateWindow(resx, resy, "Mandelbrot", 0, 0);
